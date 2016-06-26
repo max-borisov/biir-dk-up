ff2875c7d03d28af4f9337b4e1d90bae0ccf9529
max-borisov <max.borisov@yahoo.com>
2016-06-26 16:07:52 +0200

    Update images folder and add config.rb

---
+++ config.rb
@@ -0,0 +1,8 @@
+preferred_syntax = :sass
+http_path = '/'
+css_dir = 'public/css'
+sass_dir = 'src/sass'
+images_dir = 'public/images'
+javascripts_dir = 'public/javascripts'
+relative_assets = true
+line_comments = true
