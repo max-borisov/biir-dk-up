a55dce903e79703b1bd251776f18073d4d5aaf7a
max-borisov <max.borisov@yahoo.com>
2016-10-26 14:41:20 +0200

    Remove old files

--- config.rb
+++
@@ -1,8 +0,0 @@
-preferred_syntax = :sass
-http_path = '/'
-css_dir = 'public/css'
-sass_dir = 'src/sass'
-images_dir = 'public/images'
-javascripts_dir = 'public/javascripts'
-relative_assets = true
-line_comments = true
